07bc7e2e3630caaf0490e0d6918bebbf338762a8
byebyebryan <byebyebryan@gmail.com>
2015-01-31 09:03:03 -0500

    rearranged files

--- cubey/cubey.h
+++ cubey/cubey.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "Engine.h"
-#include "Camera.h"
-#include "UI.h"
-#include "Input.h"
-#include "Time.h"
+#include "cubey\Engine.h"
+#include "cubey\Camera.h"
+#include "cubey\UI.h"
+#include "cubey\Input.h"
+#include "cubey\Time.h"
 
-#include "Shader.h"
-#include "Vertex.h"
-#include "Mesh.h"
-#include "PrimitiveFactory.h"
+#include "cubey\Shader.h"
+#include "cubey\Vertex.h"
+#include "cubey\Mesh.h"
+#include "cubey\PrimitiveFactory.h"
